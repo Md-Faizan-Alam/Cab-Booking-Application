dcb3fad16c3130fbf9894fba10c5b80ea73c8595
fajju <fajjualam@gmail.com>
2022-04-21 11:58:14 +0530

    Trying to make changes

--- src/main/java/com/project/cab/CabBookingApplication.java
+++ src/main/java/com/project/cab/CabBookingApplication.java
@@ -3,6 +3,7 @@ package com.project.cab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This is the first change that made
 @SpringBootApplication
 public class CabBookingApplication {
 
