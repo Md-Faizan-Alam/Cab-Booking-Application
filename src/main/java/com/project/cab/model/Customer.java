c9c5506c543adf8260144233afbc13400e842e58
fajju <fajjualam@gmail.com>
2022-04-22 15:35:00 +0530

    Commiting all the class changes

--- src/main/java/com/project/cab/model/Customer.java
+++ src/main/java/com/project/cab/model/Customer.java
@@ -1,5 +1,40 @@
 package com.project.cab.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CUSTOMER")
 public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int customerId;
+
+	public Customer() {}
+
+	public Customer(int customerId) {
+		super();
+		this.customerId = customerId;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + "]";
+	}
+
+	
+	
 
 }
