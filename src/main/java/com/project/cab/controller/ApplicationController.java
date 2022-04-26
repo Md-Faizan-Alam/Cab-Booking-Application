4e5466e8c59cb15e87de195a2089a770ef6e9285
fajju <fajjualam@gmail.com>
2022-04-26 17:36:34 +0530

    Implementing MAV

--- src/main/java/com/project/cab/controller/ApplicationController.java
+++ src/main/java/com/project/cab/controller/ApplicationController.java
@@ -2,6 +2,7 @@ package com.project.cab.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class ApplicationController {
@@ -9,16 +10,16 @@ public class ApplicationController {
 	private static int currentUser = 0;
 	
 	@GetMapping("/")
-	public String homePage() {
-		return "home";
+	public ModelAndView homePage() {
+		return new ModelAndView("home");
 	}
 	@GetMapping("/login")
-	public String loginPage() {
-		return "login";
+	public ModelAndView loginPage() {
+		return new ModelAndView("login");
 	}
 	@GetMapping("/register")
-	public String registerPage() {
-		return "register";
+	public ModelAndView registerPage() {
+		return new ModelAndView("register");
 	}
 	
 }
