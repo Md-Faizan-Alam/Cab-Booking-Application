5bd5644e26f26967f2c16ba303da8c6673299dec
fajju <fajjualam@gmail.com>
2022-04-26 09:38:05 +0530

    Adding JSP folder

--- src/main/java/com/project/cab/controller/ApplicationController.java
+++ src/main/java/com/project/cab/controller/ApplicationController.java
@@ -1,20 +1,15 @@
 package com.project.cab.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class ApplicationController {
 	
-	@Autowired
-	JdbcTemplate jdbcTemplate;
-	
-	@RequestMapping("/")
-	public String getMsg() {
-		return "Hello from spring boot";
+	@GetMapping("/")
+	public String homePage() {
+		return "home";
 	}
 	
 }
